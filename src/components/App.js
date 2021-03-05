3fe34108787ddf0ab7d10caf4cc1923c3bb08cbe
YEONGEOL <dusrjf87@gmail.com>
2021-03-05 23:58:27 +0900

    firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import AppRouter from './Router';
+import React, { useState } from "react"
+import AppRouter from "components/Router"
+import fBase from "fBase"
 
-const App=()=>{
-  return (
-  <AppRouter />
-  );
+const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	return (
+		<>
+			<AppRouter isLoggedIn={isLoggedIn} />
+			<footer>&copy; Cuwitter {new Date().getFullYear()} Cuwitter</footer>
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,30 +1,30 @@
 // 라우터는 Component이다
 
-import React, { useState }from 'react';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom'
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import React from "react"
+import { HashRouter as Router, Route, Switch } from "react-router-dom"
+import Auth from "../routes/Auth"
+import Home from "../routes/Home"
 
-
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn]=useState(false);
-  return (
-    <Router>
-      <Switch>
-        {isLoggedIn ?
-        <>
-        {/* <> : 많은 element 들을 render 할때 사용 */}
-        <Route exact path="/">
-          <Home />
-        </Route> 
-        </> : 
-        <Route exact path="/">
-          <Auth />
-        </Route>}
-        
-      </Switch>
-    </Router>
-  );
+const AppRouter = ({ isLoggedIn }) => {
+	// { isLoggedIn } === props.isLoggedIn 구조분해문법 props객체에 들어있는 isLoggedIn 프로퍼티의 값만을 가져오는 방법
+	return (
+		<Router>
+			<Switch>
+				{isLoggedIn ? (
+					<>
+						{/* <> : 많은 element 들을 render 할때 사용 */}
+						<Route exact path="/">
+							<Home />
+						</Route>
+					</>
+				) : (
+					<Route exact path="/">
+						<Auth />
+					</Route>
+				)}
+			</Switch>
+		</Router>
+	)
 }
 
-export default AppRouter;
+export default AppRouter
