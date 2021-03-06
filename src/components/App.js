f23eff58c9f4744abf41610da0a0c514b2a718da
YEONGEOL <dusrjf87@gmail.com>
2021-03-06 23:26:37 +0900

    사용자로부터 id password 입력받는 부분 완성

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react"
 import AppRouter from "components/Router"
-import fBase from "fBase"
+import { authService } from "fBase"
 
 const App = () => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false)
+	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
 	return (
 		<>
 			<AppRouter isLoggedIn={isLoggedIn} />
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,51 @@
-import React from 'react';
+import React, { useState } from "react"
 
-const Auth = () => <span>Auth</span>;
-export default Auth;
+const Auth = () => {
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const onChange = event => {
+		const {
+			target: { name, value },
+		} = event
+		if (name === "email") {
+			setEmail(value)
+		} else if (name === "password") {
+			setPassword(value)
+		}
+	}
+
+	const onSubmit = event => {
+		event.preventDefault()
+	}
+
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					name="email"
+					type="text"
+					placeholder="이메일"
+					required
+					value={email}
+					onChange={onChange}
+				/>
+				<input
+					name="password"
+					type="password"
+					placeholder="비밀번호"
+					required
+					value={password}
+					onChange={onChange}
+				/>
+				<input type="submit" value="로그인" />
+			</form>
+			<div>
+				<button>구글 아이디로 계속하기</button>
+				<button>깃허브 아이디로 계속하기</button>
+			</div>
+		</div>
+	)
+}
+
+export default Auth
