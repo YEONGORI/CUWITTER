ab723975799a5d8be0d32b0daf73093ddbcbcb45
YEONGEOL <dusrjf87@gmail.com>
2021-03-07 23:56:25 +0900

    auth시스템 만드는즁

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from "fBase"
 import React, { useState } from "react"
 
 const Auth = () => {
 	const [email, setEmail] = useState("")
 	const [password, setPassword] = useState("")
+	const [newAccount, setNewAccount] = useState(true)
 
 	const onChange = event => {
 		const {
@@ -15,8 +17,13 @@ const Auth = () => {
 		}
 	}
 
-	const onSubmit = event => {
+	const onSubmit = async event => {
 		event.preventDefault()
+		if (newAccount) {
+			await authService.createUserWithEmailAndPassword(email, password)
+		} else {
+			// 로그인
+		}
 	}
 
 	return (
@@ -24,7 +31,7 @@ const Auth = () => {
 			<form onSubmit={onSubmit}>
 				<input
 					name="email"
-					type="text"
+					type="email"
 					placeholder="이메일"
 					required
 					value={email}
@@ -38,7 +45,7 @@ const Auth = () => {
 					value={password}
 					onChange={onChange}
 				/>
-				<input type="submit" value="로그인" />
+				<input type="submit" value={newAccount ? "회원가입" : "로그인"} />
 			</form>
 			<div>
 				<button>구글 아이디로 계속하기</button>
