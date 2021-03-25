fae740f5fb60e993afacb6687a398f6346e1c82a
YEONGEOL <dusrjf87@gmail.com>
2021-03-25 17:25:18 +0900

    useEffect return error

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,46 +1,47 @@
-import { dbService } from "fBase"
-import React, { useState, useEffect } from "react"
+import Cuweet from "components/Cuweet";
+import { dbService } from "fBase";
+import React, { useState, useEffect } from "react";
 
 const Home = ({ userObj }) => {
-	const [cuweet, setCuweet] = useState("")
-	const [cuweets, setCuweets] = useState([])
+	const [cuweet, setCuweet] = useState("");
+	const [cuweets, setCuweets] = useState([]);
 
-	// const getCuweets = async () => {
-	// 	const dbCuweets = await dbService.collection("cuweets").get()
-	// 	dbCuweets.forEach(document => {
-	// 		const cuweetObject = {
-	// 			...document.data(),
-	// 			id: document.id,
-	// 		}
-	// 		setCuweets(prev => [cuweetObject, ...prev])
-	// 	})
-	// }
+	const getCuweets = async () => {
+		const dbCuweets = await dbService.collection("cuweets").get();
+		dbCuweets.forEach(document => {
+			const cuweetObject = {
+				...document.data(),
+				id: document.id,
+			};
+			setCuweets(prev => [cuweetObject, ...prev]);
+		});
+	};
 	useEffect(() => {
 		// getCuweets()
 		dbService.collection("cuweets").onSnapshot(snapshot => {
 			const cuweetArray = snapshot.docs.map(doc => ({
 				id: doc.id,
 				...doc.data(),
-			}))
-			setCuweets(cuweetArray)
-		})
-	}, [])
+			}));
+			setCuweets(cuweetArray);
+		});
+	}, []);
 	const onSubmit = async event => {
-		event.preventDefault()
+		event.preventDefault();
 		await dbService.collection("cuweets").add({
 			text: cuweet,
 			createAt: Date.now(),
 			createrId: userObj.uid,
-		})
-		setCuweet("")
-	}
+		});
+		setCuweet("");
+	};
 
 	const onChange = event => {
 		const {
 			target: { value },
-		} = event
-		setCuweet(value)
-	}
+		} = event;
+		setCuweet(value);
+	};
 
 	return (
 		<div>
@@ -56,13 +57,11 @@ const Home = ({ userObj }) => {
 			</form>
 			<div>
 				{cuweets.map(cuweet => (
-					<div key={cuweet.id}>
-						<h4>{cuweet.text}</h4>
-					</div>
+					<Cuweet key={cuweet.id} cuweetObj={cuweet} />
 				))}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Home
+export default Home;
